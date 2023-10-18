0869826595c43bdf2afd66178123abe6cc912135
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-10-18 19:52:07 -0400

    initial commit

---
+++ application/DAO/productDAO.php
@@ -0,0 +1,19 @@
+<?php 
+
+class ProductDAO {
+    //Create
+    public function save($product){}
+    public function getAll(){}
+
+    //Retrieve
+    public function getById($id){}
+
+    //update
+    public function update($product){}
+
+    //delete
+    public function delete($id){}
+
+}
+
+?>
