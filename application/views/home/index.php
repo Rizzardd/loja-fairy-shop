fbc2c75ff7f15bc59e7edf2299d23e7a9444b0d8
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-10-20 21:05:13 -0400

    index

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,11 +1,20 @@
+<?php 
+getcwd();
+?>
+<?php include '../layout/menu.php'?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
 <body>
-    
+    <h1>bem vindo</h1>
+    <hr/>
+    <p>minha página</p>
 </body>
 </html>
