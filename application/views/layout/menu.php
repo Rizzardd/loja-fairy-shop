0bdfac6e37349c4d26755ce490056b255dcddefe
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-10-25 20:52:24 -0400

    index

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -10,13 +10,13 @@
 <body>
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
+            <a class="nav-link active" aria-current="/home/" href="#">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="/product/listar">Product</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Cart</a>
         </li>
         <li class="nav-item">
             <a class="nav-link disabled" aria-disabled="true">Disabled</a>
--- application/views/product/index.php
+++ application/views/product/index.php
@@ -7,12 +7,10 @@ include $base . '\..\layout\menu.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
 </head>
 <body>
-  
+    <h1>eres corno!</h1>
 </body>
 </html>
