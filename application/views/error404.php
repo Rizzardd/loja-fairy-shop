b21a68b1bc418dd4e648f3ee3521b21f1b43e9ce
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-10-25 20:52:18 -0400

    error 404 page

---
+++ application/views/error404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <img src="assets/img/erro.png" alt="erro">
+                <h1>Error 404</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
