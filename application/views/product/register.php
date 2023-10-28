ab61a537588e51b18017fe32a8831e2de40fc313
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-10-27 21:36:16 -0400

    register creation

---
+++ application/views/product/register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/product/store" method="POST">
+        <label for="">Product Name</label>
+        <input type="text" name="name" id="product_name">
+        <label for="">Brand Name</label>
+        <input type="text" name="brand" id="product_brand">
+        <input type="submit" value="register">
+    </form>
+</body>
+</html>
