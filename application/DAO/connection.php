0708d5373d910161840625b6bc731d990b0ddc94
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-11-08 19:52:10 -0400

    connection creation

---
+++ application/DAO/connection.php
@@ -0,0 +1,34 @@
+ <?php 
+ 
+
+ class Connection {
+    private $db_name ="loja";
+    private $db_user = "root";
+    private $db_pass = "";
+    private $db_host = "localhost";
+
+    //carrega a conexão realizada com o banco 
+    
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new \mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
+    }
+
+    public function getConnection () {
+        return $this->conn;
+        if($this->conn->connect_error) {
+            die("a conexão falhou. ". $this->conn->connect_error);
+        }
+        return $this->conn;
+    }
+    
+
+    public function connect(){}
+    public function disconnect(){
+        $this->conn->close();
+    }
+ } 
+
+
+ ?>
