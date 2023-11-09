9d7751f985b9af5a4dfe22befd2fa6e350fecc3a
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-11-08 20:38:38 -0400

    add namespace

--- application/DAO/productDAO.php
+++ application/DAO/productDAO.php
@@ -1,8 +1,24 @@
 <?php 
-
+namespace application\DAO;
 class ProductDAO {
     //Create
-    public function save($product){}
+    public function save($product){
+        $connection = new Connection(); //instancia o objeto
+        $conn = $connection->getConnection(); //recebe a conexão
+         //receber os dados da outra camada
+         $name = $product->getName();
+         $brand = $product->getBrand();
+         $price = $product->getPrice();
+        //monta o SQL
+        $SQL = "INSERT INTO product (cod, name, brand, price) values (null, $name, $brand, $price) ";
+
+        if($conn->query($SQL) === TRUE){
+            return true;
+        } else {
+            echo "Error". $SQL . "<br/>. $conn->error";
+            return false;
+        }
+    }
     public function getAll(){}
 
     //Retrieve
