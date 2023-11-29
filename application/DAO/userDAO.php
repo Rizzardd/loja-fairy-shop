42facaf928cff6e3f33f98ff55a1ea4b328ec791
rizzarddirizu <87292423+Rizzardd@users.noreply.github.com>
2023-11-29 17:06:23 -0400

    userDAO login, sign Up, update and delete creation

--- application/DAO/userDAO.php
+++ application/DAO/userDAO.php
@@ -5,25 +5,120 @@ use application\models\User;
 
 class UserDAO
 {
-    //Create
-    public function save($user)
+    // Create
+    public function signup($user)
     {
-        $connection = new Connection(); //instancia o objeto
-        $conn = $connection->getConnection(); //recebe a conexão
-        //receber os dados da outra camada
+        $connection = new Connection();
+        $conn = $connection->getConnection();
+
         $name = $user->getName();
         $email = $user->getEmail();
         $password = $user->getPassword();
-        //monta o SQL
-        $SQL = "INSERT INTO users ( name, email, password) values ( '$name', '$email', '$password') ";
-        // executa a operação
-        if ($conn->query($SQL) === TRUE) {
+
+        $SQL = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($SQL);
+        $stmt->bind_param("sss", $name, $email, $password);
+
+        if ($stmt->execute()) {
             return true;
         } else {
-            echo "Error: " . $SQL . "<br/>. $conn->error";
+            echo "Error: " . $stmt->error;
+            return false;
+        }
+    }
+
+    // Get user by ID
+    public function getById($id)
+    {
+        $connection = new Connection();
+        $conn = $connection->getConnection();
+
+        $SQL = "SELECT * FROM users WHERE cod = ?";
+        $stmt = $conn->prepare($SQL);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+
+        if ($user) {
+            return new User($user["name"], $user["email"], $user["password"]);
+        } else {
+            return null;
+        }
+    }
+    // Login
+    public function login($name, $password)
+    {
+        $connection = new Connection();
+        $conn = $connection->getConnection();
+
+        $SQL = "SELECT * FROM users WHERE email = ?";
+        $stmt = $conn->prepare($SQL);
+        $stmt->bind_param("s", $name);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+
+        if ($user && $password === $user["password"]) {
+            $_SESSION['user_id'] = $user['cod'];
+            $_SESSION['user_email'] = $user['email'];
+            $_SESSION['user_name'] = $user['name'];
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Update
+
+    public function edit($user)
+    {
+        $connection = new Connection();
+        $conn = $connection->getConnection();
+
+        $id = $_SESSION['user_id'];
+        $name = $user->getName();
+        $email = $user->getEmail();
+        $password = $user->getPassword();
+
+        $SQL = "UPDATE users SET name = ?, email = ?, password = ? WHERE cod = ?";
+        $stmt = $conn->prepare($SQL);
+        $stmt->bind_param("sssi", $name, $email, $password, $id);
+
+        if ($stmt->execute()) {
+            $_SESSION["user_name"] = $name;
+            return true;
+        } else {
+            echo "Error: " . $stmt->error;
+            return false;
+        }
+    }
+
+
+
+    // Delete
+    public function delete()
+    {
+        $connection = new Connection();
+        $conn = $connection->getConnection();
+
+        $id = $_SESSION['user_id'];
+
+        $SQL = "DELETE FROM users WHERE cod = ?";
+        $stmt = $conn->prepare($SQL);
+        $stmt->bind_param("i", $id);
+
+        if ($stmt->execute()) {
+            session_unset();
+            session_destroy();
+            return true;
+        } else {
+            echo "Error: " . $stmt->error;
             return false;
         }
     }
 }
-
 ?>
